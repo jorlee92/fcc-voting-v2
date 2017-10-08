1ca49a0a3aac5afea61e838c9072acb52ec3b1a2
jorlee92 <jorlee92@gmail.com>
2017-10-08 10:07:21 -0700

    Google charts is working now, and if you are logged in you have the sharing options

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.isAuthenticated()){
-    // res.send("You are logged in!");
-    
     res.send(req.user);
   }
   else{
